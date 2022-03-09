b6fc9027d7d9628a14d6e79d62cca1efc8d44d8a
anvy87 <anvesh87@gmail.com>
2022-03-09 07:09:53 +0530

    edits

--- PassengerCount/index.js
+++ PassengerCount/index.js
@@ -5,5 +5,12 @@ let count = 0;
 
 function increment() {
     count = count + 1;
+    // set countEl's innertext to count
     countEl.innerText = count;
+}
+
+// create a save function, which logs(print) out the count when its called
+
+function save() {
+    console.log(count);
 }
